527d5a23657450753b69e6a0c4b8f20b1beb9177
Sofia Medina <medinasofiaa1@gmail.com>
2023-05-18 11:37:05 -0300

    primer commir vectores

---
+++ scrip.js
@@ -0,0 +1,11 @@
+const semana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+
+/* semana [1] =  */
+
+console.log (semana [0]);
+
+console.log (semana [3]);
+
+console.log (semana [4]);
+
+console.log (semana [2], "semana", "String" )
