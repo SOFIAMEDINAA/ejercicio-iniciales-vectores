55eb67299823db8c74f854b354bca86571fc1e65
Sofia Medina <medinasofiaa1@gmail.com>
2023-05-18 12:06:06 -0300

    actualizacion commit vectores

--- scrip.js
+++ scrip.js
@@ -1,11 +1,31 @@
-const semana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
-
-/* semana [1] =  */
+const semana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
 
 console.log (semana [0]);
 
 console.log (semana [3]);
 
-console.log (semana [4]);
+console.log (semana [6]);
 
-console.log (semana [2], "semana", "String" )
+console.log ("el dia de la semana es " + semana [2] + " y el tipo de dato: String" );
+
+
+const animales = ["perro", "gato", "tortuga", "leon", "pajaro", "cocodrilo", "elefante"];
+
+console.log (animales [0]);
+
+console.log (animales [3]);
+
+console.log (animales [6]);
+
+console.log ("el animal es " + animales [2] + " y el tipo de dato: String " );
+
+
+const nombres = ["sofia", "johanna", "roberto", "jose", "pedro", "lucia", "julieta"];
+
+console.log (nombres [0]);
+
+console.log (nombres [3]);
+
+console.log (nombres [6]);
+
+console.log ( "el nombre es " + nombres [2] + " el tipo de dato: String" );
